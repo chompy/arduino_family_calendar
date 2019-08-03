52f025c642b1b81f4aca2d2ddd9bb9be39a5a557
Nathan Ogden <nathan@ogden.tech>
2019-08-03 14:40:07 -0400

    added GPLv3 license and README

---
+++ README.md
@@ -0,0 +1,11 @@
+Arduino Family Calendar
+-----------------------
+By Nathan Ogden
+
+A family calendar Arduino project. Displays upcoming events in the family and pictures from a micro SD card. Used Arduino + TTF shield + 3D printed case and gave to family as gifts.
+
+I made this a long time ago, so I don't know much of how any of it is supposed to work anymore. This has been uploaded to Github to demostrate my skills in C++ and electronics.
+
+## Usage
+
+In the tools directory there are several 'compile' Python2 scripts that pack assets up in to binary files. Each of these binary files should be placed in the root directory on the micro SD card. Included with this project are samples of all asset types.
--- Utils.cpp
+++ Utils.cpp
@@ -1,3 +1,20 @@
+/*
+This file is part of "arduino_family_calendar".
+
+"arduino_family_calendar" is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+"arduino_family_calendar" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with "arduino_family_calendar".  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "Utils.h"
 
 Utils::Utils(Adafruit_TFTLCD* _tft, Fat16* _file)
